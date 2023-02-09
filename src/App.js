bce9167791edb27c2bc59eba88c1c78264d33607
Vedansh bisht <35373105+ebe25@users.noreply.github.com>
2023-02-09 17:05:45 +0530

    added initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Textform from './components/Textform';
+import React, {useState} from 'react';
+
+function App() {
+   const[mode, setmode] = useState("light")
+   const toggleMode=()=>{
+    if(mode==='light'){
+      setmode('dark')
+      document.body.style.backgroundColor="rgb(15 26 59)"
+    }else{
+      setmode('light')
+      document.body.style.backgroundColor="white"
+    }
+   }
+
+  return (
+    
+  <>
+  <NavBar title="Text Utilities" mode={mode} toggleMode={toggleMode}/>
+  <div className="container">
+    {/* <About_Us/> */}
+  
+  <Textform heading="Enter Text to Manipulate:" mode={mode}/>
+
+  </div>
+
+  </>
+  );
+}
+
+export default App;
